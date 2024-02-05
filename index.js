796cf8d58be5aea4984508fa01be6f3941fd1ee8
farid95g <faridg.1995@gmail.com>
2024-02-05 14:23:50 +0400

    exercises 3.1-3.6 solutions

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+let { persons } = require('./data/index')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/persons', (_, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = +request.params.id
+    const person = persons.find(p => p.id === id)
+
+    if (!person) {
+        return response.status(404).end()
+    }
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = +request.params.id
+    persons = persons.filter(p => p.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const requiredFields = ['name', 'number']
+    const responseMessages = {
+        missing: (fields) => `The field ${fields} is required!`,
+        unique: (name) => `The name ${name} already exists in the phonebook!`
+    }
+    const body = request.body
+    
+    const missingFields = requiredFields.map(field => {
+        if (!body.hasOwnProperty(field) || body[field] === null) return field
+
+        return null
+    }).filter(value => !!value)
+
+    if (missingFields.length) {
+        return response.status(400).json({
+            error: responseMessages.missing(missingFields.join(' and '))
+        })
+    } else if (persons.find(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: responseMessages.unique(body.name)
+        })
+    }
+
+    const person = {
+        id: Math.random(),
+        ...body
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date()}</p>
+    `)
+})
+
+const PORT = 3001
+app.listen(PORT, () => `App is running on the port ${PORT}`)
