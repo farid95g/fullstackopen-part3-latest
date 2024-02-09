ce1d586bcb98eb7133e397e1a173c08f913724e8
farid95g <faridg.1995@gmail.com>
2024-02-09 16:42:28 +0400

    uibuild

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (_, response) => {
